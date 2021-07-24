882319b1a93b121cd33edf9fcf971b422f40d172
P4lmiotta <48186648+P4lmiotta@users.noreply.github.com>
2021-07-24 14:46:30 +0200

    Update List.cpp

--- List/src/List.cpp
+++ List/src/List.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : List.cpp
-// Author      : 
+// Author      : Alessandro Palmiotta
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
